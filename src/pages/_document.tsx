2adcc9466215b3d87001b047076aaf47ee26f55f
lutfifadlan <mochamadlutfifadlan@gmail.com>
2024-10-02 16:24:38 +0700

    bricolage font, update landing, init seo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,13 +8,19 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Grape+Nuts&family=Poppins:wght@300;400;600&family=Work+Sans:wght@300;400;600&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Grape+Nuts&family=Poppins:wght@300;400;600&family=Work+Sans:wght@300;400;600&family=Bricolage+Grotesque:wght@400;500;600;700&display=swap"
             rel="stylesheet"
           />
-          <meta name="description" content="Let's Focus is an all-in-one productivity app that helps you focus on what matters most." />
+          <script async src="https://cdn.seline.so/seline.js" data-token="070947bcbabaef4"></script>
+          <meta name="google-site-verification" content="bzX0de0J2dmI32xsLO5RcRb8dN5YzDW2WKZXv8cAMxY" />
+          <meta name="description" content="Let's Focus is a modern todo list app that helps you focus on what matters most." />
           <meta
             name="keywords"
             content="
+              productivity app,
+              todo list,
+              todolist,
+              todo list app,
               letsfocus,
               lets focus,
               focus,
@@ -27,7 +33,7 @@ class MyDocument extends Document {
             "
           />
           <meta property="og:title" content="Let's Focus" />
-          <meta property="og:description" content="All-in-one productivity app" />
+          <meta property="og:description" content="Let's Focus is a modern todo list app that helps you focus on what matters most." />
           <meta property="og:image" content="/logo.svg" />
           <meta property="og:url" content="https://letsfocus.today" />
           <meta property="og:type" content="website" />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,11 +9,12 @@ const config: Config = {
   ],
   theme: {
   	extend: {
-			fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
-        grapeNuts: ['Grape Nuts', 'sans-serif'],
-        workSans: ['Work Sans', 'sans-serif'],
-      },
+  		fontFamily: {
+  			poppins: ['Poppins', 'sans-serif'],
+  			grapeNuts: ['Grape Nuts', 'sans-serif'],
+  			workSans: ['Work Sans', 'sans-serif'],
+  			bricolage: ['Bricolage Grotesque', 'sans-serif'],
+  		},
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
@@ -55,28 +56,53 @@ const config: Config = {
   				'4': 'hsl(var(--chart-4))',
   				'5': 'hsl(var(--chart-5))'
   			},
-				"color-1": "hsl(var(--color-1))",
-        "color-2": "hsl(var(--color-2))",
-        "color-3": "hsl(var(--color-3))",
-        "color-4": "hsl(var(--color-4))",
-        "color-5": "hsl(var(--color-5))",
-        "green-500": "hsl(var(--green-500))", /* Add this line */
-        "red-500": "hsl(var(--red-500))", /* Add this line */
+  			'color-1': 'hsl(var(--color-1))',
+  			'color-2': 'hsl(var(--color-2))',
+  			'color-3': 'hsl(var(--color-3))',
+  			'color-4': 'hsl(var(--color-4))',
+  			'color-5': 'hsl(var(--color-5))',
+  			'green-500': 'hsl(var(--green-500))',
+  			'red-500': 'hsl(var(--red-500))'
   		},
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
   		},
-			animation: {
-				rainbow: "rainbow var(--speed, 2s) infinite linear",
-			},
-			keyframes: {
-        rainbow: {
-          "0%": { "background-position": "0%" },
-          "100%": { "background-position": "200%" },
-        },
-      },
+  		animation: {
+  			rainbow: 'rainbow var(--speed, 2s) infinite linear',
+  			'background-position-spin': 'background-position-spin 3000ms infinite alternate',
+  			shine: 'shine var(--duration) infinite linear'
+  		},
+  		keyframes: {
+  			rainbow: {
+  				'0%': {
+  					'background-position': '0%'
+  				},
+  				'100%': {
+  					'background-position': '200%'
+  				}
+  			},
+  			'background-position-spin': {
+  				'0%': {
+  					backgroundPosition: 'top center'
+  				},
+  				'100%': {
+  					backgroundPosition: 'bottom center'
+  				}
+  			},
+  			shine: {
+  				'0%': {
+  					'background-position': '0% 0%'
+  				},
+  				'50%': {
+  					'background-position': '100% 100%'
+  				},
+  				to: {
+  					'background-position': '0% 0%'
+  				}
+  			}
+  		}
   	}
   },
   // eslint-disable-next-line @typescript-eslint/no-require-imports
